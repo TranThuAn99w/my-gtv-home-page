b7c6a883f1ddf134307fca5a14bac9f5d888e3c7
TranThuAn99w <74447836+TranThuAn99w@users.noreply.github.com>
2021-08-10 09:42:36 +0700

    Add files via upload
    upload new home page

---
+++ style.js
@@ -0,0 +1,42 @@
+$('.content__imgs .item').click(function(){
+    $("#myModal").show();
+})
+$('.close').click(function(){
+    $("#myModal").hide();
+})
+$('.owl-carousel').owlCarousel({
+    items:1,
+loop:true,
+margin:10,
+nav:true,
+dots: false,
+URLhashListener:true,
+responsive:{
+0:{
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true
+}
+}
+})
+$(function() {
+$('.content__imgs').slick({
+slidesToShow: 1,
+slidesToScroll: 1,
+
+mobileFirst: true,
+arrows: true,
+dots: true,
+responsive: [
+    {
+        breakpoint: 700,
+        slidesToShow: 10,
+        settings: 'unslick'
+    }
+]
+});
+
+$(window).on('resize', function() {
+$('.content__imgs').slick('resize');
+});
+});
